17b42ba3dbc2618db034a52b3c41b39b05c065ce
gabriella garcia <gabirvgarcia@hotmail.com>
2022-01-24 22:00:00 -0300

    exercise lines deleted

--- logic.rb
+++ logic.rb
@@ -1,16 +1,3 @@
-# Dado um conjunto de caracteres, você precisará extrair os
-# "diamantes" ( <>) e as "areias" ( . ) da expressão e no
-# final exibir a quantidade de diamantes extraídos
-# Expressão: <<.<<..>><>><.>.>.<<.>.<.>>>><>><>>
-# Requisitos:
-# Extrair os diamantes e areias da expressão até que não
-# haja mais o que ser extraído;
-# Exibir a quantidade de diamantes extraídos;;
-
-# Dicas:
-# O diamante é a junção dos sinais de menor e
-# maior sem que haja entre eles
-# Areia é o ponto final
 
 str = '<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>'
 diamonds = []
